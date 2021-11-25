9d9057344f5c81a45a7cb97d36a8f3ed2c1c6597
Jānis Skabs <94110515+JanisSkabs@users.noreply.github.com>
2021-11-25 10:05:25 +0000

    kods primā daļa jeb pieprasijums

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print('Test')
+x=0
+print('Programa pieprasa figūras elementus un aprēķina to laukumu')
+print('Nospied burtu: ')
+print('T - Ja figūra ir trīstūris')
+print('Tr - Ja figūra ir trapece')
+print('P - Ja figūra ir paralelograms')
+print('Ta - Ja figūra ir taisnstūris')
+print('K - Ja figūra ir kvadrāts')
+print('R - Ja figūra ir riņķis')
+print('____________________________________________________________________________')
+
+a=input('Ievadi burtu: ')
+if a=="T":
+  print('Trīstūris')
+elif a=="Tr":
+  print('Trapece')
+elif a=="P":
+  print('paralelograms')
+elif a=="Ta":
+  print('taisnstūris')
+elif a=="K":
+  print('kvadrāts')
+elif a=="R":
+  print('riņķis')
